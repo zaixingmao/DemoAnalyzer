f6b54e1ebcba531974ff9c5ccff08764c860d2f6
Zaixing <zaixing.mao@cern.ch>
2015-07-13 09:21:00 -0500

    updates

--- test/demoanalyzer_cfg.py
+++ test/demoanalyzer_cfg.py
@@ -1,4 +1,18 @@
 import FWCore.ParameterSet.Config as cms
+import os
+
+flists = []
+flists.append([])
+counter = 0
+dirName = '/hdfs/store/mc/Phys14DR/VBF_HToTauTau_M-125_13TeV-powheg-pythia6/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v2/10000/'                                                              
+for iFile in os.listdir(dirName):                                                                                                                                                   
+    fName = 'file://' + dirName + iFile                                                                                                                                             
+    if len(flists[counter]) > 250:                                                                                                                                                  
+        flists.append([])                                                                                                                                                           
+        counter += 1                                                                                                                                                                
+    flists[counter].append(fName)                                                                                                                                                   
+print counter                                                                                                                                                                       
+
 
 process = cms.Process("Demo")
 
@@ -8,8 +22,8 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
                                 # replace 'myfile.root' with the source file you want to use
-                                fileNames = cms.untracked.vstring('file:///hdfs/store/mc/Phys14DR/VBF_HToTauTau_M-125_13TeV-powheg-pythia6/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v2/10000/6617651C-4977-E411-9156-3417EBE2F493.root'),
-          eventsToProcess = cms.untracked.VEventRange('1:2647:264697'),
+                                fileNames = cms.untracked.vstring(flists[0]),
+          eventsToProcess = cms.untracked.VEventRange('1:3050:304989', '1:3247:324687', '1:4248:424773'),
                             )
 
 process.demo = cms.EDAnalyzer('DemoAnalyzer'
